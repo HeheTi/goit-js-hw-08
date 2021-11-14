1e0c95c60bd90da4ea3a6d4da012c6f1bd11ca4c
Tetiana Zykova <tatiana.nv96@gmail.com>
2021-11-14 23:52:49 +0200

    done 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const FORM_STORAGE = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+const onSaveInfo = event => {
+  checkUserInfo[event.target.name] = event.target.value;
+  localStorage.setItem(FORM_STORAGE, JSON.stringify(checkUserInfo));
+};
+
+const takeUserInfo = JSON.parse(localStorage.getItem(FORM_STORAGE));
+const checkUserInfo = takeUserInfo ?? {};
+
+const saveData = obj => {
+  if (!obj) return;
+  document.querySelector('[name="email"]').value = obj.email;
+  document.querySelector('[name="message"]').value = obj.message;
+};
+
+const onSubmitForm = e => {
+  e.preventDefault();
+  console.log('User info:', checkUserInfo);
+  e.target.reset();
+  localStorage.removeItem(FORM_STORAGE);
+};
+
+saveData(takeUserInfo);
+refs.form.addEventListener('input', throttle(onSaveInfo, 500));
+refs.form.addEventListener('submit', onSubmitForm);
