1f15793461f3aa12c027ee22acf43c76cf32a715
Tetiana Zykova <tatiana.nv96@gmail.com>
2021-11-14 18:38:59 +0200

    done 2 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const createMarkup = ({ preview, original, description }) => `
+<div class="gallery__item">
+		<a class="gallery__link" href="${original}">
+			<img
+				class="gallery__image"
+				src="${preview}"
+				alt="${description}"
+			/>
+		</a>
+	</div>`;
+
+const renderGalleryMarckup = array => {
+  refs.gallery.innerHTML = array.map(item => createMarkup(item)).join('');
+};
+
+renderGalleryMarckup(galleryItems);
+
+new SimpleLightbox('.gallery .gallery__link', {
+  captions: true,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onTimeUpDate = e => localStorage.setItem('videoplayer-current-time', e.seconds);
+
+player.on('timeupdate', throttle(onTimeUpDate, 1000));
+
+const timeInLocalStorage = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(timeInLocalStorage)
+  .then(function (timeInLocalStorage) {})
+  .catch(error => {
+    switch (error.name) {
+      case 'RangeError':
+        console.log('The time was less than 0 or greater than the video’s duration');
+        break;
+
+      default:
+        console.log('Some other error occurred');
+        break;
+    }
+  });
